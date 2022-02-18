3c06cc682d53544209c909ff75b3d3b3dcae3545
CortezSMz <cortez_alexandre@hotmail.com>
2022-02-18 10:38:17 -0300

    refactor(theme): change theme colors acording to tabs

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,6 +1,65 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
 
+export const themes = {
+  default: {
+    light: {
+      primary: "#FF0000",
+      secondary: "#6c757d",
+      accent: "#17a2b8",
+      error: "#dc3545",
+      info: "#17a2b8",
+      success: "#28a745",
+      warning: "#FE5000",
+    },
+    dark: {
+      //
+    },
+  },
+  sempapel: {
+    light: {
+      primary: "#34b44c",
+      secondary: "#d1e6df",
+      accent: "#007bff",
+      error: "#dc3545",
+      info: "#17a2b8",
+      success: "#28a745",
+      warning: "#FE5000",
+    },
+    // TODO(themes): implement dark theme for sempapel
+    dark: {
+      primary: "#34b44c",
+      secondary: "#d1e6df",
+      accent: "",
+      error: "#dc3545",
+      info: "#17a2b8",
+      success: "#28a745",
+      warning: "#FE5000",
+    },
+  },
+  // TODO(themes): implement themes for GEMAT
+  gemat: {
+    light: {
+      primary: "#ffff00",
+      secondary: "#990000",
+      accent: "",
+      error: "#dc3545",
+      info: "#17a2b8",
+      success: "#28a745",
+      warning: "#FE5000",
+    },
+    dark: {
+      primary: "#ffff00",
+      secondary: "#990000",
+      accent: "",
+      error: "#dc3545",
+      info: "#17a2b8",
+      success: "#28a745",
+      warning: "#FE5000",
+    },
+  },
+};
+
 Vue.use(Vuetify);
 
 export default new Vuetify({
@@ -8,17 +67,7 @@ export default new Vuetify({
     options: {
       customProperties: true,
     },
-    themes: {
-      light: {
-        primary: "#36393f",
-        secondary: "#6c757d",
-        accent: "#17a2b8",
-        error: "#dc3545",
-        info: "#17a2b8",
-        success: "#28a745",
-        warning: "#FE5000",
-      },
-    },
+    themes: themes.default,
   },
   icons: {
     iconfont: "fa",
